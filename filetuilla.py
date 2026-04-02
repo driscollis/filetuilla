e60bce038139218f3bc037e9cad8c5587ba12d14
Mike Driscoll <mike@pythonlibrary.org>
2026-04-02 12:27:53 -0500

    Add more rows to UI

--- filetuilla.py
+++ filetuilla.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
-from textual.widgets import Button, Header, Input, Label, Tree
+from textual.containers import Horizontal, VerticalScroll
+from textual.widgets import Button, DirectoryTree, Header, Input
+from textual.widgets import Label, RichLog, Tree
+
 
 class FileTuilla(App):
     CSS_PATH = "filetuilla.tcss"
@@ -15,8 +19,14 @@ class FileTuilla(App):
         password.border_title = "Password"
         port = Input(id="port")
         port.border_title = "Port"
+        local_site = Input(id="local_site")
+        local_site.border_title = "Local site"
+        remote_site = Input(id="remote_site")
+        remote_site.border_title = "Remote site"
+        local_tree = DirectoryTree("/", id="local_file_tree")
+
         yield Header()
-        yield Vertical(
+        yield VerticalScroll(
             Horizontal(
                 host,
                 username,
@@ -25,6 +35,27 @@ class FileTuilla(App):
                 Button("Connect", id="connect", variant="primary"),
                 id="connection_form",
             ),
+            RichLog(id="ftp_log"),
+            Horizontal(
+                local_site,
+                remote_site,
+                id="site_inputs"
+            ),
+            Horizontal(
+                local_tree,
+                Tree("Remote", id="remote_file_tree"),
+                id="tree_row"
+            ),
+            # File info data tables
+
+            # File info row (number of files/directories, total size)
+            Horizontal(
+                Label("local", id="local_file_info"),
+                Label("remote", id="remote_file_info"),
+                id="file_info_row",
+            ),
+            # Transfer info
+
             id="main_container"
         )
 
