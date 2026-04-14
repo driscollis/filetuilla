ad117d82735bf069629fb55a025c5079c1b8cad6
Mike Driscoll <mike@pythonlibrary.org>
2026-04-14 07:55:43 -0500

    Add download capability

--- sftp_utils.py
+++ sftp_utils.py
@@ -2,8 +2,20 @@ import paramiko
 from pathlib import Path
 
 
+def download_file(
+    remote_file: str, local_dir: Path, ftp_client: paramiko.sftp_client.SFTPClient
+) -> None:
+    """
+    Download a file over SFTP
+    """
+    ftp_client.get(remote_file, local_dir.joinpath(Path(remote_file).name))
+
+
 def upload_file(
     local_file: Path, remote_file: str, ftp_client: paramiko.sftp_client.SFTPClient
 ) -> None:
+    """
+    Upload a file over SFTP
+    """
     # Upload a file
     ftp_client.put(local_file, remote_file)
